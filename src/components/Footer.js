cc3a65b41f849abba649a9cbbaa05217d42d05e5
Lili Wilson <56806227+liliwilson@users.noreply.github.com>
2023-12-28 21:32:16 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ export default function Footer() {
       <Grid container spacing={1}>
         <Grid item xs={4} sm={5}></Grid>
         <Grid item xs={4} sm={2}>
-          <a
-            href="https://www.instagram.com/whateversonjupiter"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <InstagramIcon fontSize="large" sx={{ color: "black" }} />
-          </a>
+          // <a
+          //   href="https://www.instagram.com/whateversonjupiter"
+          //   target="_blank"
+          //   rel="noreferrer"
+          // >
+          //   <InstagramIcon fontSize="large" sx={{ color: "black" }} />
+          // </a>
           <a
             href="https://www.linkedin.com/in/maia-wilson-317161285/"
             target="_blank"
