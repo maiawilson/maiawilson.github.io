7473b9a77eff63f673d442751403bbaa6a78184f
Maia Wilson <104437032+maiawilson@users.noreply.github.com>
2025-08-31 17:15:32 -0400

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -15,15 +15,15 @@ export default function AboutPage() {
                 Hi! I’m Maia Wilson, I'm from New York City, and I’m currently a first-year at Yale University.
                 <br />
                 <br />
-                I’ve been creating drawings non-stop since I was a child,
-                scribbling characters in crayon on whatever surface I could find. 
+                I’ve been creating drawings nonstop since I was a child,
+                scribbling doodles on whatever surface I could find. 
                 I filled out sketchbook after sketchbook with characters from my
                 imagination and attempts at capturing real life.
                 <br />
                 <br />
                 After attending a summer program at Cooper Union, a whole new set 
                 of media was opened up to me. I fell in love with inks, charocal, 
-                oil paints, and even pastels. At a RISD printmaking program two
+                oil paints, and pastels. At a RISD printmaking program two
                 summers later, I learned the art of screenprinting. Both programs
                 allowed me to exhibit my art in a formal setting. I won a 
                 Scholastic Art and Writers Awards gold key for one of these exhibition 
@@ -33,13 +33,12 @@ export default function AboutPage() {
                 In my junior year, I took Advanced Placement (AP) Drawing and
                 experimented with cut-paper techniques and honed in on exploring
                 the beauty of my close bond with my sisters. In senior year, I took
-                AP Art History to have more references to draw on for my art. 
+                AP Art History and exposed myself to more artistic references. 
                 <br />
                 <br />
-                I’m planning on incorporating some of the engineering knowledge
-                I’ve learned in robotics to bring to life moving sculptures and
-                dolls based on some of the characters that I’ve been sketching
-                all these years.
+                Now, I'm pursuing a STEM degree and hoping to incorporate technology
+                into my art. I'm experimenting with short film projects, paper dolls,
+                moving sculptures, and as always, tons of sketching. 
               </Typography>
             </Box>
           </Grid>
