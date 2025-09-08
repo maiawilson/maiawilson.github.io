1c051e68ae2367347ef1f11713253e642e1936be
Maia Wilson <104437032+maiawilson@users.noreply.github.com>
2025-09-08 14:45:36 -0400

    Update CVPage.js

--- src/pages/CVPage.js
+++ src/pages/CVPage.js
@@ -9,7 +9,7 @@ export default function CVPage() {
       <NavBar darkText={true} />
       <Box m={5} px={10}>
         <iframe
-          src="images/MaiaWilsonCV.pdf"
+          src="images/MaiaWilsonCV.png"
           height="600em"
           width="60%"
           title="resume"
